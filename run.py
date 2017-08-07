be69676a9dd3c4373ab417d861b39b083d87ffec
jessamynsmith <jessamyn.smith@gmail.com>
2017-08-07 19:08:32 -0400

    Added vote counter.

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import func
 
 app = Flask(__name__)
 
@@ -57,8 +58,14 @@ def homepage():
         else:
             message_level = 'danger'
             message = 'You must enter your email and select a fighter to cast a vote.'
-    fighters = Fighter.query.all()
-    return render_template('index.html', message=message, message_level=message_level, fighters=fighters)
+
+    fighters = Fighter.query.order_by('id').all()
+    total_votes = db.session.query(Vote).count()
+    vote_query = db.session.query(Vote.fighter_id, func.count(Vote.fighter_id))
+    vote_counts = vote_query.group_by(Vote.fighter_id).order_by('fighter_id').all()
+
+    return render_template('index.html', message=message, message_level=message_level,
+                           fighters=fighters, total_votes=total_votes, vote_counts=vote_counts)
 
 
 if __name__ == '__main__':
